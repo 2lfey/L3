cf1045340412567fd3507a50414b6e1c4fe29425
ilfey <ilfey.global@gmail.com>
2023-11-27 21:31:13 +0300

    update ui

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+from django.utils.timezone import utc
+
+from datetime import datetime
+
 # Create your models here.
 
 
@@ -17,3 +21,38 @@ class Post(models.Model):
     content = models.TextField(max_length=10000, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def time(self):
+        now = datetime.utcnow().replace(tzinfo=utc)
+
+        if (self.updated_at - self.created_at).seconds > 3:
+            diff = (now - self.updated_at)
+            if diff.days == 0:
+                # Today 00:00
+                return self.created_at.strftime('Edited today %H:%M')
+            elif diff.days < 7:
+                # Monday 00:00
+                return self.created_at.strftime('Edited %A %H:%M')
+            elif diff.days < 365:
+                # November 00:00
+                return self.created_at.strftime('Edited %B %H:%M')
+            else:
+                # 2023 November 00:00
+                return self.created_at.strftime('Edited %Y %B %H:%M')
+        else:
+            diff = (now - self.created_at)
+            if diff.days == 0:
+                # Today 00:00
+                return self.created_at.strftime('Today %H:%M')
+            elif diff.days < 7:
+                # Monday 00:00
+                return self.created_at.strftime('%A %H:%M')
+            elif diff.days < 365:
+                # November 00:00
+                return self.created_at.strftime('%B %H:%M')
+            else:
+                # 2023 November 00:00
+                return self.created_at.strftime('%Y %B %H:%M')
+
+    def __str__(self) -> str:
+        return self.content
---
+++ static/js/toggleTheme.js
@@ -0,0 +1,13 @@
+
+const toggler = document.querySelector("#toggleTheme")
+const element = document.documentElement
+
+toggler.addEventListener('mouseup', () => {
+  if (element.classList.contains("dark")) {
+    element.classList.remove("dark")
+    localStorage.setItem("theme", "light")
+  } else {
+    element.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  }
+})
